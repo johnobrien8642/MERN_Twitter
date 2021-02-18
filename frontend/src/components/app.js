0baed245848896131e681ad5cedf62ceaf7efa5a
your name here <johnobrien8642@gmail.com>
2021-02-17 21:49:36 -0500

    Load frontend in production

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
-
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import TweetsContainer from './tweets/tweets_container';
 import ProfileContainer from './profile/profile_container';
 import TweetComposeContainer from './tweets/tweet_compose_container';
+const path = require('path')
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const App = () => (
   <div>
